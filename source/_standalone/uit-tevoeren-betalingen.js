eeb0110c4ec310b421d0e527f056fd52854bd09e
Timo Hofmeijer <timo@NL2-THR.local>
2017-06-13 09:39:55 +0200

    ‘Uit te voeren betalingen’ specification

---
+++ source/_standalone/uit-tevoeren-betalingen.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+  type: 'Page.Collection',
+  collection: {
+    actions: [
+      ['Inschrijven', 'arrowRight', 'primary']
+    ],
+    count: 3,
+    model: {
+      title: 'Uit te voeren betaling',
+      titlePlural: 'Uit te voeren betalingen',
+      data: [
+        {
+          title: 'Datum',
+          sampleData: 'date'
+        },
+        {
+          title: 'Crediteur',
+          sampleData: '_organisation'
+        },
+        {
+          title: 'Bron',
+          sampleData: ['integer', {min: 10000, max: 99999}, ['Verkoopfactuur ']]
+        },
+        {
+          title: 'Totaal',
+          sampleData: ['euro', {min: 100, max: 9999}]
+        },
+        {
+          title: 'Betaald',
+          sampleData: ['euro', {min: 100, max: 9999}]
+        },
+        {
+          title: 'Saldo',
+          sampleData: ['euro', {min: 100, max: 9999}]
+        }
+      ]
+    }
+  }
+}
